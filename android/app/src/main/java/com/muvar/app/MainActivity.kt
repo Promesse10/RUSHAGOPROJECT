215d964dea28287ef3df5aa94aa102cbbca31b34
unknown <promesseirakoze10@gmail.com>
2026-02-10 10:51:52 +0200

    Fix Android SDK versions and regenerate native code

--- android/app/src/main/java/com/muvar/app/MainActivity.kt
+++ android/app/src/main/java/com/muvar/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.helloworld
+package com.muvar.app
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/muvar/app/MainApplication.kt
+++ android/app/src/main/java/com/muvar/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.helloworld
+package com.muvar.app
 
 import android.app.Application
 import android.content.res.Configuration
